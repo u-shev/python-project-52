8d7684f21077c12fb0c7e33f71ccd6c97945d3c7
u-shev <sheveleva.jul@gmail.com>
2023-08-23 20:02:37 +0300

    step 1.1 	modified:   .gitignore 	modified:   poetry.lock 	modified:   pyproject.toml 	modified:   task_manager/settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0m^g@2f@0y4by5&p*g=&f8%4orf3!pmwtk!y(jzl3jam$!2j0j'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
