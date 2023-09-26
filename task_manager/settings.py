726e48001ce03ba8a1c2d68f8a6dab5f0abae1eb
u-shev <sheveleva.jul@gmail.com>
2023-09-26 13:35:35 +0300

    changed design

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -87,17 +87,17 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        conn_max_age=600,
-        default='sqlite:///db.sqlite3')
-}
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
+#     'default': dj_database_url.config(
+#         conn_max_age=600,
+#         default='sqlite:///db.sqlite3')
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
